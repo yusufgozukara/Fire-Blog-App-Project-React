eb7e205246ac361256900c45101d5aebbb20f467
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-28 13:38:56 +0300

    dashboard title added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div>
-      Start
+      <Dashboard/>
     </div>
   );
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="dashboard">
+        <p className="dashboardLine"></p>
+        <Typography
+          variant="h2"
+          margin="0 10px"
+          textAlign="center"
+          color="primary"
+          display="inline-block"
+          fontFamily="Girassol"
+        >
+          DashboarD
+        </Typography>
+        <p className="dashboardLine"></p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
