6777f4e97bcbbdfa5dea2b735eaec42940ba0f0e
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-30 22:11:12 +0300

    New Blog Page Added

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -4,7 +4,9 @@ import Navbar from '../components/Navbar'
 import About from '../pages/About'
 import Dashboard from '../pages/Dashboard'
 import Login from '../pages/Login'
+import NewBlog from '../pages/NewBlog'
 import NotFound from '../pages/NotFound'
+import Profile from '../pages/Profile'
 import Register from '../pages/Register'
 
 const AppRouter = () => {
@@ -16,6 +18,8 @@ const AppRouter = () => {
           <Route path='/about' element={<About/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/newblog' element={<NewBlog/>}/>
           <Route path='/' element={<Dashboard/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
--- src/pages/NewBlog.js
+++ src/pages/NewBlog.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import blok from '../assets/blok.png';
+import { TextareaAutosize } from '@mui/material';
 
-const NewBlog = () => {
+function Copyright(props) {
   return (
-    <div>NewBlog</div>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default NewBlog
+const theme = createTheme();
+
+export default function NewBlog() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <img src={blok} alt="blok" />
+          
+          <Typography component="h1" variant="h5" >
+            New Blog
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="title"
+              placeholder="Title"
+              name="title"
+              autoComplete="title"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="image URL"
+              placeholder="Image URL"
+              type="text"
+              id="image"
+              autoComplete="current-password"
+            />
+            <TextareaAutosize
+              margin="normal"
+              required
+              fullWidth
+              minRows={15}
+              maxRows={20}
+              style={{ width: 500 }}
+              name="Content"
+              label="Content"
+              type="text"
+              id="Content"
+              placeholder="Content"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+
+      </Container>
+    </ThemeProvider>
+  );
+}
