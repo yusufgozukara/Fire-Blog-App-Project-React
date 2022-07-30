23917012c6b4782f747bfd0be5ef669ec92a90d5
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-30 14:49:32 +0300

    BrowserRouter designed

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import About from '../pages/About'
 import Dashboard from '../pages/Dashboard'
+import NotFound from '../pages/NotFound'
 
 const AppRouter = () => {
   return (
@@ -9,11 +11,12 @@ const AppRouter = () => {
     <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
 
         
-        <Dashboard/>
     </BrowserRouter>
     </>
   )
