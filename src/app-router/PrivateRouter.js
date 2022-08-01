8292c3e2fa8f8521aa08b3294535490256eb34a3
yusufgozukara <yusufgozukara11@gmail.com>
2022-08-01 23:12:44 +0300

    private router-v1

--- src/app-router/PrivateRouter.js
+++ src/app-router/PrivateRouter.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRouter = () => {
-    const user = false;
+    const user = true;
 
   return user ? <Outlet/> : <Navigate to='/login'/>
 }
